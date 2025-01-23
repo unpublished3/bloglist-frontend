ff0572eeb61db7551cdb80665db1b5f5bcf98733
unpublished3 <chapagainsakshyam@gmail.com>
2025-01-23 12:02:53 +0545

    Complete 5.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,72 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const blogList = () => {
+    return (
+      <div>
+        <h2>Blogs</h2>
+        <div>{user.name} logged in</div>
+        {blogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+      </div>
+    );
+  };
+
+  const loginForm = () => {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            Username
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+
+          <div>
+            Password
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  };
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
-  return (
-    <div>
-      <h2>Blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+  return user ? blogList() : loginForm();
+};
 
-export default App
+export default App;
---
+++ src/components/Blogs.jsx
@@ -0,0 +1,6 @@
+
+const Blogs = ({ blogs }) => {
+
+};
+
+export default Blogs;
