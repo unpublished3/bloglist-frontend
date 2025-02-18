a3cd97e8de95196d1b9df3b1b84f3f0f898333ba
unpublished3 <chapagainsakshyam@gmail.com>
2025-02-18 12:56:18 +0545

    Complete 5.14

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -16,7 +16,7 @@ describe("<Blog />", () => {
     render(<Blog blog={blog} />)
   })
 
-  test("title and author are visible by default but likes and url is not", async () => {
+  test("title and author are visible by default but likes and url is not", () => {
     expect(screen.getByText("Testing Blogs")).not.toHaveStyle("display: none")
     expect(screen.getByText("by Blog Author")).not.toHaveStyle("display: none")
 
@@ -26,5 +26,19 @@ describe("<Blog />", () => {
     expect(
       screen.getByText("Url: https://blogs.com/tests").closest("div")
     ).toHaveStyle("display: none")
+  })
+
+  test("likes and url are visible after 'View' button is clicked", async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText("View")
+    await user.click(button)
+
+    expect(screen.getByText("Likes: 999").closest("div")).not.toHaveStyle(
+      "display: none"
+    )
+
+    expect(
+      screen.getByText("Url: https://blogs.com/tests").closest("div")
+    ).not.toHaveStyle("display: none")
   })
 })
